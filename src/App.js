2591d90cde9f9f7406b65708296f2ed81441bdd5
Sergii <sergii.khylko@outlook.com>
2023-03-10 18:00:44 +0200

    #8 create UserList component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import CreateUser from "./Components/Users/CreateUser";
+import UserList from "./Components/Users/UserList";
 
 const App = () => {
   return (
     <div>
       <CreateUser />
+      <UserList users={[]} />
     </div>
   );
 };
