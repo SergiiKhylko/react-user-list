15085c808a1c1ebc7ea54d0d9377c1354684f755
Sergii <sergii.khylko@outlook.com>
2023-03-10 17:31:45 +0200

    #7 add form handlers and simple validations

--- src/Components/Users/CreateUser.js
+++ src/Components/Users/CreateUser.js
@@ -1,24 +1,60 @@
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 import styles from "./CreateUser.module.css"
+import React, {useState} from "react";
 
 const CreateUser = props => {
 
+  const [inputName, setInputName] = useState("");
+  const [inputAge, setInputAge] = useState("");
+
+  const nameChangeHandler = event => {
+    setInputName(event.target.value);
+  }
+
+  const ageChangeHandler = event => {
+    setInputAge(event.target.value);
+  }
+
   const createUserHandler = event => {
     event.preventDefault();
-    console.log("submit");
+
+    if (inputName.trim().length === 0 || inputAge.trim().length === 0) {
+      console.log("Data is not valid");
+      return;
+    }
+    if (+inputAge < 1) {
+      console.log("Age cannot be negative");
+      return;
+    }
+
+    console.log("submit " +  inputName + " " + inputAge);
+    setInputName("");
+    setInputAge("");
   }
 
   return (
     <Card className={styles.input}>
       <form onSubmit={createUserHandler} >
         <label htmlFor="name">Name</label>
-        <input id="name" type='text' />
+        <input
+          id="name"
+          type='text'
+          onChange={nameChangeHandler}
+          value={inputName}
+        />
         <label htmlFor="age">Age</label>
-        <input id="age" type='number' />
-        <button type="submit">Add User</button>
+        <input
+          id="age"
+          type='number'
+          onChange={ageChangeHandler}
+          value={inputAge}
+        />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
 }
+
 
 export default CreateUser;
